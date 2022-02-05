f473350719435e02f65bbf32650482d489add20f
Jady Sobjak de Mello Godoi <jady.s.m@gmail.com>
2022-02-04 23:06:09 -0300

    quinta aula dart week (metade)

---
+++ lib/app/modules/order/shopping_card/shopping_card_bindings.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import './shopping_card_controller.dart';
+
+class ShoppingCardBindings implements Bindings {
+  @override
+  void dependencies() {
+    Get.put(ShoppingCardController(
+      authService: Get.find(),
+      shoppingCardService: Get.find(),
+    ));
+  }
+}
---
+++ lib/app/modules/order/shopping_card/shopping_card_controller.dart
@@ -0,0 +1,40 @@
+import 'package:get/get.dart';
+import 'package:vakinha_burguer_mobile/app/core/services/auth_service.dart';
+import 'package:vakinha_burguer_mobile/app/core/services/shopping_card_service.dart';
+import 'package:vakinha_burguer_mobile/app/models/shopping_card_model.dart';
+
+class ShoppingCardController extends GetxController {
+  final AuthService _authService;
+  final ShoppingCardService _shoppingCardService;
+
+  final _address = ''.obs;
+  final _cpf = ''.obs;
+
+  ShoppingCardController({
+    required AuthService authService,
+    required ShoppingCardService shoppingCardService,
+  })  : _authService = authService,
+        _shoppingCardService = shoppingCardService;
+
+  List<ShoppingCardModel> get products => _shoppingCardService.products;
+  double get totalValue => _shoppingCardService.totalValue;
+
+  set address(String address) => _address.value = address;
+  set cpf(String cpf) => _cpf.value = cpf;
+
+  void addQuantityInProduct(ShoppingCardModel shoppingCardModel) {
+    _shoppingCardService.addAndRemoveProductInShoppingCard(
+      shoppingCardModel.product,
+      quantity: shoppingCardModel.quantity + 1,
+    );
+  }
+
+  void subtractQuantityInProduct(ShoppingCardModel shoppingCardModel) {
+    _shoppingCardService.addAndRemoveProductInShoppingCard(
+      shoppingCardModel.product,
+      quantity: shoppingCardModel.quantity - 1,
+    );
+  }
+
+  void clear() => _shoppingCardService.clear;
+}
